532853381b4a5012ccd2ed9a7d790f7913856da1
murali putta <>
2023-02-06 15:35:54 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,10 @@
+## Band Name Generator
+
+print(" Welcome to the Band Name Generator.\n")
+
+city = input("What's name of the city you grew up in?\n")
+pet_name = input("What's your pet's name?\n")
+
+band_name = city + " "+pet_name
+
+print(f'Your band name could be {band_name}')
